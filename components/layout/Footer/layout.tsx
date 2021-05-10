9117375df7eb61728a7b75585b31da50ae82453d
arbenger <arbenger@git.com>
2021-05-10 17:40:50 +0800

    added pattern to footer

--- components/layout/Footer/layout.tsx
+++ components/layout/Footer/layout.tsx
@@ -1,4 +1,5 @@
 import { styled, Box } from "@material-ui/core";
+import { brickWall } from "hero-patterns";
 
 export const Container = styled(Box)(({ theme }) => {
   const { customPalette } = theme;
@@ -10,11 +11,12 @@ export const Container = styled(Box)(({ theme }) => {
 });
 
 export const FeatureOneContainer = styled(Box)(({ theme }) => {
-  const { customPalette, spacing, breakpoints } = theme;
+  const { customPalette, spacing } = theme;
   const { current } = customPalette;
 
   return {
     backgroundColor: customPalette[current].main,
+    backgroundImage: brickWall(customPalette[current].contrastText, 0.07),
     padding: `${spacing(4)}px ${spacing(1)}px`,
     textAlign: "left",
   };
@@ -26,6 +28,7 @@ export const FeatureTwoContainer = styled(Box)(({ theme }) => {
 
   return {
     backgroundColor: customPalette[current].dark,
+    backgroundImage: brickWall(customPalette[current].contrastText, 0.07),
     textAlign: "center",
     padding: spacing(2),
   };
